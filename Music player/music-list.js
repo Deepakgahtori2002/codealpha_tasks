047386319dce2ef3c4daf0ce0290bbba862c16c7
Deepakgahtori2002 <deep2001gahtori@gmail.com>
2025-02-11 12:13:12 +0530

    files added

---
+++ Music player/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Chandni",
+        artist: "Sachet Tandon,ParamPara Tandon ",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Ishq Ka Asar",
+        artist: "Stebin Ben",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Taaron Ke Shehar",
+        artist: "Neha Kakkar",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Teri Aankhon Mein",
+        artist: "Darshan Raval",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Teri Baat Aur Hai",
+        artist: "Stebin Ben",
+        img: "music-5",
+        src: "music-5"
+    },
+]
